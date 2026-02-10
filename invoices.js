4eadfdcb59688f42c1fe5795253bd892b8f354ac
Berkant Gunes <gunesberkant96@gmail.com>
2026-02-10 17:05:12 +0300

    Add files via upload

---
+++ invoices.js
@@ -0,0 +1,30 @@
+/**
+ * @type {import('mongoose').Model}
+ */
+
+const Invoice = require("../models/Invoice.js");
+const express = require("express");
+const router = express.Router();
+
+// get all invoices
+router.get("/get-all", async (req,res) => {
+    try {
+        const invoices = await Invoice.find();
+        res.status(200).json(invoices)
+    } catch(error) {
+        res.status(500).json(error);
+    }
+})
+
+router.post("/add-invoice", async (req, res)=> {
+    try {
+        const newInvoice = new Invoice(req.body)
+        await newInvoice.save();
+        res.status(200).json("Item added successfully.");
+    }
+    catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+module.exports = router;
