39b92f6b6470d2c8d2e2c101e57d46410063d8ba
Berkant Gunes <gunesberkant96@gmail.com>
2026-02-10 17:05:26 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,48 @@
+/**
+ * @type {import('mongoose').Model}
+ */
+
+const Product = require("../models/Product.js");
+const express = require("express");
+const router = express.Router();
+
+// get all products
+router.get("/get-all", async (req,res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json(products)
+    } catch(error) {
+        res.status(500).json(error);
+    }
+})
+
+router.post("/add-product", async (req, res)=> {
+    try {
+        const newProduct = new Product(req.body)
+        await newProduct.save();
+        res.status(200).json("Item added successfully.");
+    }
+    catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+router.put("/update-product", async (req,res) => {
+    try {
+        await Product.findOneAndUpdate({ _id: req.body._id }, req.body);
+        res.status(200).json("Item updated successfully")
+    } catch(error) {    
+        res.status(500).json(error)
+    }
+})
+
+router.delete("/delete-product", async (req,res) => {
+    try {
+        await Product.findOneAndDelete({ _id: req.body._id });
+        res.status(200).json("Item deleted successfully")
+    } catch(error) {    
+        res.status(500).json(error);
+    }
+})
+
+module.exports = router;
